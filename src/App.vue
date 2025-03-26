eb6cbdc626731261ff516efde592950c5ea63fbd
AppCreator <orisio90@gmail.com>
2025-03-26 23:14:41 +0700

    start

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <AutoDiagnostics />
+</template>
+
+<script>
+import AutoDiagnostics from './components/AutoDiagnostics.vue';
+
+export default {
+  components: {
+    AutoDiagnostics
+  }
+};
+</script>
+<style>
+html, body {
+  background: url('src/assets/background.jpg') no-repeat center center fixed;
+  background-size: cover;
+  margin: 0;
+  padding: 0;
+  height: 100%;
+  width: 100%;
+}
+</style>
